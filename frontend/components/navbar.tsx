019164edd11f159338e9165899cbe79a18f4ed12
hafidzyami <18221074@std.stei.itb.ac.id>
2024-06-03 18:07:28 +0700

    fix layout

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -8,7 +8,7 @@ function NavigationBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Joki Arrifqi</Navbar.Brand>
+        <Navbar.Brand href="#home">Weather Forecast</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
